a375d6369d6734c7b31b77e822fe9d5c13a63240
march-dave <ysangy@gmail.com>
2019-06-08 10:30:26 -0400

    First Init

---
+++ __tests__/App.spec.js
@@ -0,0 +1,25 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import { shallow} from 'enzyme'
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper  = shallow(<App />)
+} )
+
+it('jsfld' , () => {
+  expect(wrapper.find('#hello')).toHaveLength(1)
+})
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
