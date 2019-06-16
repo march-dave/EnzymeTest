63a0246665d794e73ea3c82ff522954ecec736f0
march-dave <ysangy@gmail.com>
2019-06-15 20:03:45 -0400

    readme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 2) setup 파일을 만들고 설정파일(config, Adapter를)만든다.
 3) __tests__ 파일 폴더에 작업을 시작한다.
 4) __tests__ 의 파일 이름은 .spec.js 로 되어 있는 파일 테스트 파일이다.
+5) beforeEach에 이것 저것 글로벌 변수 처럼 추가 한다.
+6) 여기에 enzyme 의 shallow 를 쓴다.
+7) 
--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -8,7 +8,7 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import { shallow} from 'enzyme'
+import { shallow } from 'enzyme'
 
 let wrapper;
 
